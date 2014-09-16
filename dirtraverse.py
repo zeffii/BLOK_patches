f847c4f2b21cd1950fa68cd1d562f22e2c059521
zeffii <tetha.z@gmail.com>
2014-09-16 09:30:50 +0200

    drudg

--- dirtraverse.py
+++ dirtraverse.py
@@ -1,18 +1,23 @@
-import os
 
+# import os
+# rootDir = "."
+# fileSet = set()
 
-# for dirname, dirnames, filenames in os.walk('.'):
-#     if dirname[2:].startswith('.'):
-#         continue
-#     if dirnames:
-#         for subdir in dirnames:
-#             path = (dirname[2:], subdir)
-#             print(path)
+# mega_dict = {}
 
+# for dir_, _, files in os.walk(rootDir):
+#     for fileName in files:
+#         relDir = os.path.relpath(dir_, rootDir)
+#         relFile = os.path.join(relDir, fileName)
+#         if not relFile.startswith('.'):
+#             fileSet.add(relFile)
+
+# print(sorted(fileSet))
 
 import os
 rootDir = "."
 fileSet = set()
+
 
 for dir_, _, files in os.walk(rootDir):
     for fileName in files:
